06d9b4470f5663a6fc325d9237dedb3366429acf
Paranut Wongsanupa <44996497+ZisZtR@users.noreply.github.com>
2026-02-03 16:51:26 +0000

    connect pgadmin, create compose

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -4,6 +4,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm # use to see the progress in loading chunk
+import click
 
 dtype = {
     "VendorID": "Int64",
@@ -30,19 +31,31 @@ parse_dates = [
 ]
 
 
-def run():
-    year = 2021
-    month = 1
 
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--table-name', default='yellow_taxi_data', help='Target table name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--chunk-size', default=100000, type=int, help='Chunk size fro reading CSV')
 
-    chunk_size = 100000
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, table_name, year, month, chunk_size):
 
-    table_name = 'yellow_taxi_data'
+    # year = 2021
+    # month = 1
+
+    # pg_user = 'root'
+    # pg_pass = 'root'
+    # pg_host = 'localhost'
+    # pg_port = 5432
+    # pg_db = 'ny_taxi'
+    # table_name = 'yellow_taxi_data'
+
+    # chunk_size = 100000
 
     # Read a sample of the data
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
@@ -78,7 +91,7 @@ def run():
             is_first = False
         
         df_chunk.to_sql(
-            name='yellow_taxi_data', 
+            name=f'{table_name}_{year}_{month}', 
             con=engine, 
             if_exists='append')
 # df = next(df_iter) # next iter
